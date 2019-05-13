b05c2d02ba5e62f0dd0d0de8488c30cebb68ff7e
chenwen <chenwen@lastmiles.cn>
2019-05-13 14:24:21 +0800

    项目初始化

--- src/page/login.vue
+++ src/page/login.vue
@@ -1,29 +1,56 @@
 <template>
     <div class="login">
-      123
+        <el-row>
+            <el-col :span="24">
+                <div class="headerlogo">
+                    <div class="headerlogo-logo">
+                        <img src="../assets/login_03.png" alt="" width="103" height="83">
+                    </div>
+                </div>
+                <div class="box">
+
+                </div>
+            </el-col>
+        </el-row>
     </div>
 </template>
 
 <script>
- export default {
-   data () {
-        return {
+    export default {
+        data() {
+            return {
 
-       }
-   },
-   components: {
+            }
+        },
+        components: {
 
-   },
-   created(){
+        },
+        created() {
 
-   },
-   methods: {
+        },
+        methods: {
 
-   },
- }
+        },
+    }
 </script>
 
 <style scoped lang='less'>
+.headerlogo{
+    width: 40%;
+    min-width: 400px;
+    height: 90px;
+    background-color: red;
+    .headerlogo-logo{
+        width: 330px;
+        height: 90px;
+        background-color: blue;
+        float: right;
+    }
+}
+.box{
+    width: 100%;
+    height: 789px;
+    background-color: pink;
+}
 
- 
 </style>
